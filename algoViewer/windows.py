2014eeb342fc5f1418e9aff0297f109a7b983887
alexisraelmtz <alex.israel.mtz@gmail.com>
2021-07-19 00:16:28 -0500

    Algorithms Modules Integrated Issue #05

--- algoViewer/windows.py
+++ algoViewer/windows.py
@@ -2,6 +2,7 @@ import pygame
 import math
 from queue import PriorityQueue
 import time
+from aStarSearch import aStarSearch as first
 
 
 WIDTH = 600
@@ -101,66 +102,6 @@ class Node:
 #################################
 
 
-def h(p1, p2):
-    x1, y1 = p1
-    x2, y2 = p2
-    return abs(x1 - x2) + abs(y1 - y2)
-
-
-def tracePath(cameFrom, current, draw):
-    while current in cameFrom:
-        current = cameFrom[current]
-        current.makePath()
-        draw()
-
-
-def aStarAlgo(draw, graph, start, end):
-    count = 0
-    openSet = PriorityQueue()
-    openSet.put((0, count, start))
-    cameFrom = {}
-    gScore = {node: float("inf") for row in graph for node in row}
-    gScore[start] = 0
-    fScore = {node: float("inf") for row in graph for node in row}
-    fScore[start] = h(start.getPosition(), end.getPosition())
-
-    openSetDict = {start}
-
-    while not openSet.empty():
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-
-        current = openSet.get()[2]
-        openSetDict.remove(current)
-
-        if current == end:
-            tracePath(cameFrom, end, draw)
-            end.makeEnd()
-            return True
-
-        for neighbour in current.neighbours:
-            gTempScore = gScore[current] + 1
-
-            if gTempScore < gScore[neighbour]:
-                cameFrom[neighbour] = current
-                gScore[neighbour] = gTempScore
-                fScore[neighbour] = gTempScore + \
-                    h(neighbour.getPosition(), end.getPosition())
-                if neighbour not in openSetDict:
-                    count += 1
-                    openSet.put((fScore[neighbour], count, neighbour))
-                    openSetDict.add(neighbour)
-                    neighbour.makeOpen()
-        # time.sleep(0.1)
-        draw()
-
-        if current != start:
-            current.makeClosed()
-
-    return False
-
-
 def makeGraph(rows, width):
     graph = []
     size = width // rows
@@ -248,8 +189,8 @@ def main(window, width):
                     for row in graph:
                         for node in row:
                             node.updateNeighbours(graph)
-                    aStarAlgo(lambda: draw(window, graph,
-                                           ROWS, width), graph, start, end)
+                    first.aStarSearch(lambda: draw(window, graph,
+                                                   ROWS, width), graph, start, end)
 
                 if event.key == pygame.K_c:
                     start = None
