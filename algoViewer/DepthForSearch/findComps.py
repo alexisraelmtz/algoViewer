1715cc68d0e556e42e9e6a60760a22826b4d4682
alexisraelmtz <alex.israel.mtz@gmail.com>
2021-10-22 12:00:11 -0500

    Validating global variables - sintax errors fixed

--- algoViewer/DepthForSearch/findComps.py
+++ algoViewer/DepthForSearch/findComps.py
@@ -4,26 +4,28 @@
 
 graph = [
     # adjacency list
-    [1, 2],             # 0
-    [0, 2, 3],          # 1
-    [0, 1, 3],          # 2
-    [1, 2],             # 3
-    [5, 9],             # 4
-    [4, 9, 10, 6],      # 5
+    [1, 2],  # 0
+    [0, 2, 3],  # 1
+    [0, 1, 3],  # 2
+    [1, 2],  # 3
+    [5, 9],  # 4
+    [4, 9, 10, 6],  # 5
     [7, 8, 11, 10, 5],  # 6
-    [6],                # 7
-    [6],                # 8
-    [4, 5],             # 9
-    [11, 5, 6],         # 10
-    [6, 18, 12, 10],    # 11
-    [11, 18],           # 12
-    [16, 14],           # 13
-    [13, 16, 15],       # 14
-    [14],               # 15
-    [13, 17, 14],       # 16
-    [16],               # 17
-    [12, 11],           # 18
+    [6],  # 7
+    [6],  # 8
+    [4, 5],  # 9
+    [11, 5, 6],  # 10
+    [6, 18, 12, 10],  # 11
+    [11, 18],  # 12
+    [16, 14],  # 13
+    [13, 16, 15],  # 14
+    [14],  # 15
+    [13, 17, 14],  # 16
+    [16],  # 17
+    [12, 11],  # 18
 ]
+
+
 size = len(graph)
 count = 0
 components = [0 for _ in range(size)]
@@ -47,9 +49,7 @@ def depthForSearch(node):
     neighbours = graph[node]
     for next in neighbours:
         if not visited[next]:
-            print(
-                f"Currently at Node {node}: {neighbours}. Visiting {next}"
-            )
+            print(f"Currently at Node {node}: {neighbours}. Visiting {next}")
             depthForSearch(next)
 
 
